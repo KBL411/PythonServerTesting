33523a5d10c19888f32271124dc3fd6c9b134879
Rayan Edjekouane <rayan.edjekouane@efrei.net>
2021-11-30 18:16:56 +0100

    Mean function

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-import time
-from flask import Flask
+import flask
+import numpy as np
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "hello to my sample flask ap"
+    return "hello to my flask app, to test the mean function copy/paste this in tour webrowser\n " \
+           "localhost:5000/mean?list=1&list=2&list=3&list=4 "
+
+
+@app.route('/mean', methods=['GET'])
+def mean():
+    da_list = request.args.getlist("list", type=int)
+    list_mean = np.mean(da_list)
+    return "The list: '{}' \n The mean '{}'".format(da_list, list_mean)
+
